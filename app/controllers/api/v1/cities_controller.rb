ae582535f98bf12ffa331234f381fe7a602fdd37
horizon-team-inc <horizon.team.toronto@gmail.com>
2025-02-21 17:00:17 -0500

    Add checks for current page and limit per page

--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -2,7 +2,12 @@ class Api::V1::CitiesController < ApplicationController
   
   # GET /api/v1/cities
   def index
-    @cities = City.order(name: :ASC, country_code: :ASC)
+    # Check if request has limit parameter
+    per_page = params[:limit].present? ? params[:limit].to_i : 10
+    per_page = 50 if per_page > 50
+
+    @cities = City.page(params[:page]).per(per_page)
+      .order(name: :ASC, country_code: :ASC)
     render json: @cities
   end
 end
